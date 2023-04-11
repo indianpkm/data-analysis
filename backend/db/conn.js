cd64f0b18a487cf1a6e77c4c3b8c9f8164f999a3
PRAMOD KUMAR MAURYA <115163168+indianpkm@users.noreply.github.com>
2023-04-11 12:44:41 +0100

    Add files via upload

---
+++ backend/db/conn.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+const username=process.env.DB_USERNAME;
+const password=process.env.DB_PASSWORD;
+
+const URL=`mongodb+srv://${username}:${password}@cluster1.xoficl5.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', true);
+const Connection=async ()=>{
+    try{
+       await mongoose.connect(URL , {useUnifiedTopology:true});
+       console.log('connection successful')
+    }catch(err){
+        console.log('error with db connection',err.message);
+    }
+}
+
+export default Connection;
